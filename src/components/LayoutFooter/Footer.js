646155a142489827cbd919fee622c04d35648969
TY LO DUNG <doan.trong.quoc.binh@gmail.com>
2019-06-22 18:50:11 +0700

    up

--- src/components/LayoutFooter/Footer.js
+++ src/components/LayoutFooter/Footer.js
@@ -61,7 +61,7 @@ const Footer = ({layoutHasSidebar = false}) => (
           }}>
           <FooterNav layoutHasSidebar={layoutHasSidebar}>
             <MetaTitle onDark={true}>Tài trợ & Công Đức</MetaTitle>
-            <FooterLink to="/blog/">Mẹo Vặt Thường Ngày</FooterLink>
+            <FooterLink to="/blog/">Công Nghệ OME</FooterLink>
             <ExternalFooterLink
               href="http://bakadono.com"
               target="_blank"
